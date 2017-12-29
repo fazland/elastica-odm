24b0608ea3f4582bec147f9324c4a839bf48dab1
Massimiliano Braglia <massimiliano.braglia@fazland.com>
2017-12-29 11:42:55 +0100

    DatabaseTest and CollectionTest

---
+++ tests/Collection/CollectionTest.php
@@ -0,0 +1,97 @@
+<?php declare(strict_types=1);
+
+namespace Fazland\ODM\Elastica\Tests\Collection;
+
+use Elastica\Query;
+use Elastica\ResultSet;
+use Elastica\Scroll as ElasticaScroll;
+use Elastica\Search as ElasticaSearch;
+use Elastica\SearchableInterface;
+use Fazland\ODM\Elastica\Collection\Collection;
+use Fazland\ODM\Elastica\Collection\CollectionInterface;
+use Fazland\ODM\Elastica\DocumentManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+
+class CollectionTest extends TestCase
+{
+    /**
+     * @var DocumentManagerInterface|ObjectProphecy
+     */
+    private $documentManager;
+
+    /**
+     * @var SearchableInterface|ObjectProphecy
+     */
+    private $searchable;
+
+    /**
+     * @var Query|ObjectProphecy
+     */
+    private $query;
+
+    /**
+     * @var string
+     */
+    private $documentClass;
+
+    /**
+     * @var CollectionInterface
+     */
+    private $collection;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        $this->documentManager = $this->prophesize(DocumentManagerInterface::class);
+        $this->searchable = $this->prophesize(SearchableInterface::class);
+        $this->query = $this->prophesize(Query::class);
+        $this->documentClass = \stdClass::class;
+
+        $this->collection = new Collection(
+            $this->documentManager->reveal(),
+            $this->documentClass,
+            $this->searchable->reveal()
+        );
+    }
+
+    public function testScrollShouldSetDefaultSortingIfNotSet(): void
+    {
+        $search = $this->prophesize(ElasticaSearch::class);
+        $this->searchable->createSearch($this->query)->shouldBeCalled()->willReturn($search);
+
+        $this->query->hasParam('sort')->willReturn(false);
+        $this->query->setSort(['_doc'])->shouldBeCalled();
+
+        $expiryTime = '1m';
+
+        $search->scroll($expiryTime)->willReturn($this->prophesize(ElasticaScroll::class));
+        $this->collection->scroll($this->query->reveal(), $expiryTime);
+    }
+
+    public function testSearchShouldExecuteTheQuery(): void
+    {
+        $this->searchable->search($this->query)
+            ->shouldBeCalled()
+            ->willReturn($this->prophesize(ResultSet::class))
+        ;
+
+        $this->collection->search($this->query->reveal());
+    }
+
+    public function testCreateSearchShouldWork(): void
+    {
+        $search = $this->collection->createSearch($this->query->reveal());
+
+        $this->assertEquals($this->query->reveal(), $search->getQuery());
+    }
+
+    public function testCountShouldUseSearchableInterfaceCount(): void
+    {
+        $this->searchable->count($this->query)->shouldBeCalled()->willReturn(10);
+
+        $this->collection->count($this->query->reveal());
+    }
+}
--- tests/Collection/DatabaseTest.php
+++ tests/Collection/DatabaseTest.php
@@ -4,32 +4,91 @@ namespace Fazland\ODM\Elastica\Tests\Collection;
 
 use Elastica\Client;
 use Elastica\Index;
+use Fazland\ODM\Elastica\Collection\CollectionInterface;
 use Fazland\ODM\Elastica\Collection\Database;
 use Fazland\ODM\Elastica\DocumentManagerInterface;
+use Fazland\ODM\Elastica\Metadata\DocumentMetadata;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prophecy\ObjectProphecy;
+use Psr\Cache\CacheItemPoolInterface;
 
 class DatabaseTest extends TestCase
 {
+    /**
+     * @var Client|ObjectProphecy
+     */
+    private $client;
+
+    /**
+     * @var DocumentManagerInterface|ObjectProphecy
+     */
+    private $documentManager;
+
+    /**
+     * @var Database
+     */
+    private $database;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        $this->client = $this->prophesize(Client::class);
+        $this->documentManager = $this->prophesize(DocumentManagerInterface::class);
+
+        $this->database = new Database($this->client->reveal(), $this->documentManager->reveal());
+    }
+
     public function testGetIndexWithAliasShouldReturnTheSameIndex(): void
     {
-        /** @var Client|ObjectProphecy $client */
-        $client = $this->prophesize(Client::class);
-        /** @var Client|ObjectProphecy $documentManager */
-        $documentManager = $this->prophesize(DocumentManagerInterface::class);
-
-        $database = new Database($client->reveal(), $documentManager->reveal());
-
         $index = 'index';
         $alias1 = 'alias1';
         $alias2 = 'alias2';
 
-        $database->addAlias($alias1, $index);
-        $database->addAlias($alias2, $index);
+        $this->database->addAlias($alias1, $index);
+        $this->database->addAlias($alias2, $index);
 
-        $client->getIndex($index)->willReturn($this->prophesize(Index::class))->shouldBeCalledTimes(2);
+        $this->client->getIndex($index)->willReturn($this->prophesize(Index::class))->shouldBeCalledTimes(2);
 
-        $database->getIndex($alias1);
-        $database->getIndex($alias2);
+        $this->database->getIndex($alias1);
+        $this->database->getIndex($alias2);
+    }
+
+    public function testGetCollectionShouldReturnACollectionWithTheDatabaseCache(): void
+    {
+        $class = new DocumentMetadata(new \ReflectionClass(\stdClass::class));
+        $class->name = 'document_name';
+        $class->typeName = 'type_name';
+
+        $index = $this->prophesize(Index::class);
+
+        $this->client->getIndex($class->typeName)->willReturn($index);
+
+        $resultCache = $this->prophesize(CacheItemPoolInterface::class);
+
+        $this->database->setResultCache($resultCache->reveal());
+
+        $collection = $this->database->getCollection($class);
+        $this->assertInstanceOf(CollectionInterface::class, $collection);
+
+        $this->assertEquals($resultCache->reveal(), $collection->getResultCache());
+    }
+
+    public function testGetCollectionCalledMoreThanOnceShouldRetrieveTheSameCollectionInstance(): void
+    {
+        $class = new DocumentMetadata(new \ReflectionClass(\stdClass::class));
+        $class->name = 'document_name';
+        $class->typeName = 'type_name';
+
+        $index = $this->prophesize(Index::class);
+
+        $this->client->getIndex($class->typeName)->willReturn($index);
+
+        $collection = $this->database->getCollection($class);
+
+        $collection2 = $this->database->getCollection($class);
+
+        $this->assertEquals($collection, $collection2);
     }
 }
