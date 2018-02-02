5a22489177431064be54fe6f171407db92e5d79c
Alessandro Chitolina <alessandro.chitolina@fazland.com>
2018-02-02 16:48:04 +0100

    wrong namespace

--- src/Command/DropSchemaCommand.php
+++ src/Command/DropSchemaCommand.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Elastica\Command;
+namespace Fazland\ODM\Elastica\Command;
 
 use Fazland\ODM\Elastica\DocumentManagerInterface;
 use Fazland\ODM\Elastica\Metadata\DocumentMetadata;
