3025ba2958cfab7467a0b7bcbb6dfdb698c1d876
Alessandro Chitolina <alessandro.chitolina@fazland.com>
2018-03-08 11:29:38 +0100

    add filter expression option to update schema command

--- src/Command/UpdateSchemaCommand.php
+++ src/Command/UpdateSchemaCommand.php
@@ -6,6 +6,7 @@ use Fazland\ODM\Elastica\DocumentManagerInterface;
 use Fazland\ODM\Elastica\Tools\SchemaGenerator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -29,6 +30,7 @@ class UpdateSchemaCommand extends Command
     protected function configure()
     {
         $this->setName('update-schema');
+        $this->addOption('filter-expression', null, InputOption::VALUE_REQUIRED, 'Filters the classes for schema updates via a regular expression');
     }
 
     /**
@@ -42,7 +44,26 @@ class UpdateSchemaCommand extends Command
         $generator = new SchemaGenerator($this->documentManager);
         $schema = $generator->generateSchema();
 
+        $expression = $input->getOption('filter-expression');
+        if (null !== $expression) {
+            if (false === preg_match($expression, '')) {
+                throw new \InvalidArgumentException('Filter expression is not a valid regex');
+            }
+
+            $filter = function ($value) use ($expression): bool {
+                return (bool) preg_match($expression, $value);
+            };
+        } else {
+            $filter = function (): bool {
+                return true;
+            };
+        }
+
         foreach ($schema->getMapping() as $className => $mapping) {
+            if (! $filter($className)) {
+                continue;
+            }
+
             $collection = $this->documentManager->getCollection($className);
             $collection->updateMapping($mapping->getMapping());
         }
